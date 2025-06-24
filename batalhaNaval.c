c1132fdaff8f2ac424384b19d607304b4cedec2a
S0muelss <samuelalexandre241@gmail.com>
2025-06-24 17:07:30 +0000

    navios na diagonal adicionado juntos com a verificação

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,19 +5,9 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main (){
-//declaração para criar o tabulerio
+
 char tabuleiro [10][10] ; // matriz para o tabuleiro
-int c, l; //variáveis para linhas e colunas
-// declaração de navios
-int navioH [3] = {3,3,3}; // navio horizontal ocupando 3 posições
-int navioV [3] =  {3,3,3}; // navio vertical ocupando 3 posições
-
-// cordenadas dos navio
-int linhaH = 1; // linha horizontal do navio
-int colH  = 3; // coluna horizontal do navio
-
-int linhaV = 3;   // Linha inicial do navio vertical
-int colV = 6;     // Coluna do navio vertical
+int c, l; 
 
 
 for (c=0; c < 10; c++){ //para percorrer as colunas do tabuleiro
@@ -27,13 +17,48 @@ for (c=0; c < 10; c++){ //para percorrer as colunas do tabuleiro
 }
 
 // posicionando o navio horizontal
- for (int i = 0 ; i<3; i++){
- 	tabuleiro[linhaH  ][colH+i]  = '3';
+ int linhaH = 1;//linha horizontal do navio
+ int colH = 3; //coluna do navio
+ for(int i = 0; i<3;i++){
+	if (tabuleiro[linhaH][colH + i] == '3'){
+		 printf("Erro: sobreposição no navio horizontal.\n");
+            return 1;
+	}
+	 tabuleiro[linhaH][colH + i] = '3';
  }
 //posicionando os navios na vertical
-for (int i = 0 ; i<3; i++){
- 	tabuleiro[linhaV + i][colV] = '3';
-}
+ int linhaV = 3; // linha vertical do navio
+ int colV = 6; // coluna vertical do navio
+ for(int i = 0; i < 3; i++){
+	if(tabuleiro[linhaV+i][colV]) == '3'{
+		 printf("Erro: sobreposição no navio vertical.\n");
+            return 1;
+	}
+	tabuleiro[linhaV + i][colV] = '3';
+ }
+
+ // ======= NAVIO DIAGONAL PRINCIPAL ======= (linha e coluna aumentam)
+    int linhaD1 = 5;
+    int colD1   = 1;
+    for (int i = 0; i < 3; i++) {
+        if (tabuleiro[linhaD1 + i][colD1 + i] == '3') {
+            printf("Erro: sobreposição na diagonal principal.\n");
+            return 1;
+        }
+        tabuleiro[linhaD1 + i][colD1 + i] = '3';
+    }
+
+    // ======= NAVIO DIAGONAL SECUNDÁRIA ======= (linha aumenta, coluna diminui)
+    int linhaD2 = 0;
+    int colD2   = 9;
+    for (int i = 0; i < 3; i++) {
+        if (tabuleiro[linhaD2 + i][colD2 - i] == '3') {
+            printf("Erro: sobreposição na diagonal secundária.\n");
+            return 1;
+        }
+        tabuleiro[linhaD2 + i][colD2 - i] = '3';
+    }
+
 
 //imprimir o tabulerio[
 printf("   "); // para alinha as linhas com as colunas
